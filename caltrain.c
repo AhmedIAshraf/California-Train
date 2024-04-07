0a00e1a91d326c254b3242e0ee1b0c2814c7e285
AhmedIAshraf <121035001+AhmedIAshraf@users.noreply.github.com>
2024-04-07 23:10:57 +0200

    Update caltrain.c

--- caltrain.c
+++ caltrain.c
@@ -6,19 +6,20 @@
 void
 station_init(struct station *station)
 {
-	station->emptySeats=0;
-	station->waitingPassengers=0;
-	station->passengersOnTrain=0;
-	pthread_mutex_init(&(station->mutex),NULL);
-	pthread_cond_init(&(station->waitTrainWithEmptySeats),NULL);
-	pthread_cond_init(&(station->waitPassengers),NULL);
+    station->emptySeats=0;
+    station->waitingPassengers=0;
+    station->passengersOnTrain=0;
+    pthread_mutex_init(&(station->mutex),NULL);
+    pthread_cond_init(&(station->waitTrainWithEmptySeats),NULL);
+    pthread_cond_init(&(station->waitPassengers),NULL);
     pthread_cond_init(&(station->onBoard),NULL);
 }
 
 void
 station_load_train(struct station *station, int count)
 {
-	station->emptySeats=count;
+    pthread_mutex_lock(&(station->mutex));
+    station->emptySeats=count;
 //    printf("Train arrived with %d seats, Current Waiting is %d\n",station->emptySeats,station->waitingPassengers);
     if (count>0){
         pthread_cond_broadcast(&(station->waitTrainWithEmptySeats));
@@ -26,6 +27,7 @@ station_load_train(struct station *station, int count)
             pthread_cond_wait(&(station->waitPassengers),&(station->mutex));
         }
     }
+    pthread_mutex_unlock(&(station->mutex));
 }
 
 void
